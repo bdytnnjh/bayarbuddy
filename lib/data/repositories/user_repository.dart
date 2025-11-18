2f35371e40cf08b02d298b6eb0cfcb5b1eb22a2f
sendiagustian <sendiagustian.pk@gmail.com>
2025-11-18 22:02:26 +0700

    added feature trusted contact

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,20 @@
+import 'package:app/core/configs/firebase_query.dart';
+import 'package:flutter/foundation.dart';
+
+class UserRepository {
+  static const String _usersCollection = 'users';
+
+  Future<bool> checkUserExistsByEmail(String email) async {
+    try {
+      final usersSnapshot = await FirebaseQuery.getDocuments(
+        collection: _usersCollection,
+        queryBuilder: (collection) => collection.where('email', isEqualTo: email).limit(1),
+      );
+
+      return usersSnapshot.docs.isNotEmpty;
+    } catch (e) {
+      debugPrint('Check User Exists Error: $e');
+      rethrow;
+    }
+  }
+}
